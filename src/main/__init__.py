5a51be55ba59e71606069f93119aead28c877adc
Martin Skarzynski <marskar@gmail.com>
2018-12-31 20:59:30 -0500

    add main example

--- src/main/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for Main."""
-
-__author__ = """Martin Skarzynski"""
-__version__ = '0.0.1'
-
---
+++ src/main/__main__.py
@@ -0,0 +1,5 @@
+def print_name_and_file():
+    print('__name__ is', __name__, 'and __file__ is', __file__)
+
+if __name__ == "__main__":
+    print_name_and_file()
