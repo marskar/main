98092fbecc5bfe48246132c2c09c3f942dc884ce
Martin Skarzynski <marskar@gmail.com>
2019-01-22 22:21:57 -0500

    Added: src/

---
+++ src/pkg/__main__.py
@@ -0,0 +1,5 @@
+def print_name_and_file():
+    print('Name is', __name__, 'and file is', __file__)
+
+if __name__ == "__main__":
+    print_name_and_file()
---
+++ src/pkg/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""Main module."""
+
+def print_name_and_file():
+    print('Name is', __name__, 'and file is', __file__)
+
+if __name__ == "__main__":
+    print_name_and_file()
