0a72ff0a8d279a83b60f3871fca8d773e4215e18
Martin Skarzynski <marskar@gmail.com>
2019-01-22 21:10:37 -0500

    Deleted: src/main/__main__.py Modified: src/main/main.py

--- src/main/__main__.py
+++
@@ -1,5 +0,0 @@
-def print_name_and_file():
-    print('__name__ is', __name__, 'and __file__ is', __file__)
-
-if __name__ == "__main__":
-    print_name_and_file()
--- src/main/main.py
+++ src/main/main.py
@@ -3,5 +3,8 @@
 
 """Main module."""
 
-def main(arg1: str) -> None:
-    print(arg1)
+def print_name_and_file():
+    print('__name__ is', __name__, 'and __file__ is', __file__)
+
+if __name__ == "__main__":
+    print_name_and_file()
